5807ea828bbd85e888a3e1eb7d45a65990c88604
waniyakhxn12 <108590308+waniyakhxn12@users.noreply.github.com>
2022-07-03 00:03:41 +0500

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void hotel(){
+	
+}
+
+void receptionist(){
+	
+}
+
+void rooms(){
+	
+}
+
+void user(){
+	char name[20]; char gender; int age;
+	printf("Enter Full Name: ");
+	scanf("%s", name);
+	printf("Enter Gender[m/f]: ");
+	scanf("%c", &gender);
+	printf("Enter Age: ");
+	scanf("%d", &age);
+	char id[10] = "kjl";
+}
+
+int main() {
+  char arr[20] = "sherry";
+	char age = 1;
+	char gender = 'm';
+	char dj[40];
+	dj[0] = arr[0];
+	dj[1] =  gender;
+	dj[2] = age;
+	puts(dj);
+}
